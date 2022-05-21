110847df40dee62a9e64cef40252b2eee33b414d
Ramez Rafla <ramez@zegenie.com>
2022-05-21 07:35:54 -0400

    fixed symlink

--- deps/symlink.js
+++ deps/symlink.js
@@ -7,13 +7,13 @@ const source = path.resolve(path.sep, process.argv[3] || path.join(__dirname, 's
 const filenames = process.argv.slice(4).map(str => path.basename(str));
 
 /*
-	This creates symlinks inside the <$2> directory, linking to files inside the
-	directory specified by the absolute path <$3>. If no path <$3> is provided,
-	the default path of "./deps/sqlite3" is used. The basenames of the files to
-	link are specified by <$4...>.
+  This creates symlinks inside the <$2> directory, linking to files inside the
+  directory specified by the absolute path <$3>. If no path <$3> is provided,
+  the default path of "./deps/sqlite3" is used. The basenames of the files to
+  link are specified by <$4...>.
  */
 
 for (const filename of filenames) {
-	fs.accessSync(path.join(source, filename));
-	fs.symlinkSync(path.join(source, filename), path.join(dest, filename), 'file');
+  fs.accessSync(path.join(source, filename));
+  fs.copyFileSync(path.join(source, filename), path.join(dest, filename));
 }
